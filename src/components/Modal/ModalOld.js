55026d33e362719f6f64524692beceedfb86a4ee
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2023-09-15 16:24:39 +0300

    fox4

---
+++ src/components/Modal/ModalOld.js
@@ -0,0 +1,47 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Overlay, ModalWindow } from './Modal.styled';
+
+export class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+    document.body.style.overflow = 'hidden';
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+    document.body.style.overflow = 'visible';
+  }
+
+  handleKeyDown = event => {
+    if (event.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  handleBackdropClick = event => {
+    if (event.currentTarget === event.target) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    const { largeImageURL, tags } = this.props;
+
+    return (
+      <Overlay onClick={this.handleBackdropClick}>
+        <ModalWindow>
+          <img src={largeImageURL} alt={tags} />
+        </ModalWindow>
+      </Overlay>
+    );
+  }
+}
+
+Modal.propTypes = {
+  largeImageURL: PropTypes.string.isRequired,
+  tags: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default Modal;
