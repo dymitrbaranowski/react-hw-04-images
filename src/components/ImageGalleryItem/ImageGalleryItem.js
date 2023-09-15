0030dff001f088c86d89a16e678511e1a0cc3506
dymitrbaranowski <dmitribaranowski51@gmail.com>
2023-09-15 11:55:06 +0300

    fox2

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,42 +1,17 @@
+import { useState } from 'react';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Modal from '../Modal/Modal';
 import { Item, Img } from './ImageGalleryItem.styled';
 
-class ImageItem extends Component {
-  state = {
-    showModal: false,
+export const ImageItem = ({ Img }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(prevModal => !prevModal); // Инвертирует значение showModal
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
-  };
 
-  render() {
-    const { showModal } = this.state;
-    const { image } = this.props;
-
-    return (
-      <>
-        <Item>
-          <Img
-            src={image.webformatURL}
-            alt={image.tags}
-            onClick={this.toggleModal}
-          />
-          {showModal && (
-            <Modal
-              largeImageURL={image.largeImageURL}
-              tags={image.tags}
-              onClose={this.toggleModal}
-            />
-          )}
-        </Item>
-      </>
-    );
-  }
 }
 
 ImageItem.propTypes = {
