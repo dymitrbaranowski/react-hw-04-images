e7b8f1be34f573b2377f2607f8759ed1e9619fe2
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2023-09-15 15:10:46 +0300

    fox3

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,15 +1,33 @@
 import { useState } from 'react';
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Modal from '../Modal/Modal';
 import { Item, Img } from './ImageGalleryItem.styled';
 
-export const ImageItem = ({ Img }) => {
+export const ImageItem = ({ image }) => {
   const [showModal, setShowModal] = useState(false);
 
   const toggleModal = () => {
     setShowModal(prevModal => !prevModal); // Инвертирует значение showModal
   };
+
+  return (
+    <>
+      <Item>
+        <Img
+          src={image.webformatURL}
+          alt={image.tags}
+          onClick={toggleModal}
+        />
+        {showModal && (
+          <Modal
+            largeImageURL={image.largeImageURL}
+            tags={image.tags}
+            onClose={toggleModal}
+          />
+        )}
+      </Item>
+    </>
+  );
 
 
 }
